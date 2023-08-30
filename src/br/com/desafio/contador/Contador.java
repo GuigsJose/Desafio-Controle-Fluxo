60de8a03e787aa2ac3652d2c1071841e7a2424c8
Guilherme Jose <guizaomtz@gmail.com>
2023-08-30 19:59:50 -0300

    second commit

--- src/br/com/desafio/contador/Contador.java
+++ src/br/com/desafio/contador/Contador.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Contador {
+package br.com.desafio.contador;
+
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = sc.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = sc.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        }catch (ParametrosInvalidosException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+            int contagem = parametroDois - parametroUm + 1;
+            for (int i = parametroUm; i <= parametroDois; i++){
+            System.out.println("Imprimindo o número " + i);
+        }
+     }
 }
+
--- src/br/com/desafio/contador/ParametrosInvalidosException.java
+++ src/br/com/desafio/contador/ParametrosInvalidosException.java
@@ -1,2 +1,6 @@
-package br.com.desafio.contador;public class ParametrosInvalidosException {
+package br.com.desafio.contador;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String s) {
+    }
 }
